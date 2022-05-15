c88ad8cdcd682c74c0c720fc3d5cc94922ca7109
avagyansamvel <avagyansamvel07@gmail.com>
2022-05-15 18:32:50 +0400

    Add game board, add Draw function that takes left and top arguments of the board and draw the given symbol.

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -4,9 +4,25 @@ namespace ConsoleGame
 {
     class Program
     {
+        static void Draw(int left, int top, char c)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.Write(c);
+        }
+
         static void Main(string[] args)
         {
+            int boardWidth = 30;
+            int boardHeight = 20;
+            int[,] board = new int[boardHeight, boardWidth];
 
+            for(int i = 0; i < boardHeight; i++)
+            {
+                for(int j = 0; j < boardWidth; j++)
+                {
+                    Draw(i + 1, j + 1, board[i, j].ToString()[0]);
+                }
+            }
         }
     }
 }
